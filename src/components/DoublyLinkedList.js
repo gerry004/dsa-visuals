a24f4ecdc311f3d914a211e9e44250a2189af05d
Gerry Yang <gerry04y@gmail.com>
2023-10-22 13:37:04 +0100

    Doubly Linked List

--- src/components/DoublyLinkedList.js
+++ src/components/DoublyLinkedList.js
@@ -1,118 +1,100 @@
 import { useEffect, useState } from "react"
 
-function DoublyLinkedList() {
-  const [nodes, setNodes] = useState([])
+class DoublyLinkedListNode {
+  constructor(data, next, prev) {
+    this.data = data
+    this.next = next
+    this.prev = prev
+  }
+}
 
-  const data = [1, 2]
+function DoublyLinkedList() {
+  const [tail, setTail] = useState(null)
+  const [head, setHead] = useState(null)
+  const [render, setRender] = useState(false)
+
+  const data = [1, 2, 3, 4]
+
+  let currentHead = head
+  let currentTail = tail
 
   useEffect(() => { initialiseDoublyLinkedList() }, [])
 
   const initialiseDoublyLinkedList = () => {
     if (data.length === 0) return
-    const nodes = []
-    const initialNode = {
-      data: data[0],
-      next: null,
-      prev: null
-    }
-    let currentNode = initialNode
-    nodes.push(initialNode)
-    for (let i = 1; i < data.length; i++) {
-      const newNode = {
-        data: data[i],
-        next: null,
-        prev: currentNode
+    for (let i = 0; i < data.length; i++) {
+      if (currentHead === null && currentTail === null) {
+        insertIntoEmptyList(data[i])
       }
-      currentNode.next = newNode
-      currentNode = currentNode.next
-      nodes.push(newNode)
-    }
-    setNodes(nodes)
-  }
-  const printList = () => {
-    let head = nodes[0]
-    let currentNode = head
-    let currentPos = 0
-    while (currentNode !== null) {
-      console.log("element", currentPos, currentNode.next, currentNode.prev)
-      currentNode = currentNode.next
-      currentPos++
-    }
-  }
-
-  const insertAtBeginning = () => {
-    if (nodes.length === 0) {
-      const data = 140
-      const newNode = {
-        data: data,
-        next: null,
-        prev: null
+      else {
+        const newNode = new DoublyLinkedListNode(data[i], null, currentTail)
+        currentTail.next = newNode
+        currentTail = newNode
       }
-      setNodes([newNode])
-      return
     }
-    const data = 140
-    const head = nodes[0]
-    const newNode = {
-      data: data,
-      next: head,
-      prev: null
-    }
-    head.prev = newNode
-    setNodes([newNode, ...nodes])
+    setHead(currentHead)
+    setTail(currentTail)
   }
 
-  const insertAtEnd = () => {
-    if (nodes.length === 0) {
-      const data = 80
-      const newNode = {
-        data: data,
-        next: null,
-        prev: null
+  const insertIntoEmptyList = (data) => {
+    const newNode = new DoublyLinkedListNode(data, null, null)
+    currentHead = newNode
+    currentTail = newNode
+  }
+
+  const insertAtBeginning = (data) => {
+    if (currentHead === null && currentTail === null) {
+      insertIntoEmptyList(data)
+    } else {
+      const newNode = new DoublyLinkedListNode(data, currentHead, null)
+      currentHead.prev = newNode
+      currentHead = newNode
+    }
+    setHead(currentHead)
+    setTail(currentTail)
+  }
+
+  const insertAtEnd = (data) => {
+    if (currentHead === null && currentTail === null) {
+      insertIntoEmptyList(data)
+    } else {
+      const newNode = new DoublyLinkedListNode(data, null, currentTail)
+      currentTail.next = newNode
+      currentTail = newNode
+    }
+    setHead(currentHead)
+    setTail(currentTail)
+  }
+
+  const insertBefore = (data, position) => {
+    if (head === null && tail === null) {
+      insertIntoEmptyList(data)
+    } else {
+      let currentNode = currentHead
+      let currentPos = 0
+      while (currentNode !== null && currentPos < position) {
+        currentNode = currentNode.next
+        currentPos++
       }
-      setNodes([newNode])
-      return
-    }
-    const data = 80
-    const tail = nodes[nodes.length - 1]
-    const newNode = {
-      data: data,
-      next: null,
-      prev: tail
-    }
-    tail.next = newNode
-    setNodes([...nodes, newNode])
-  }
-
-  const insertBefore = () => {
-    if (nodes.length === 0) return
-    const position = 1
-    const data = 100
-    const head = nodes[0]
-    let currentNode = head
-    let currentPos = 0
-    while (currentNode !== null && currentPos < position) {
-      currentNode = currentNode.next
-      currentPos++
-    }
-    if (currentNode != null && currentPos === position) {
-      const newNode = {
-        data: data,
-        next: currentNode,
-        prev: currentNode.prev
+      if (currentPos === position) {
+        if (currentNode.prev === null) {
+          insertAtBeginning(data)
+        } else if (currentNode.prev !== null && currentNode.next !== null) {
+          const newNode = new DoublyLinkedListNode(data, currentNode, currentNode.prev)
+          currentNode.prev.next = newNode
+          currentNode.prev = newNode
+        } else {
+          insertIntoEmptyList(data)
+        }
       }
-      currentNode.prev.next = newNode
-      currentNode.prev = newNode
-
-      setNodes([...nodes.slice(0, position), newNode, ...nodes.slice(position)])
     }
+    setHead(currentHead)
+    setTail(currentTail)
   }
 
-  const deleteAt = () => {
-    if (nodes.length === 0) return
-    const position = 0
-    const head = nodes[0]
-    let currentNode = head
+  const deleteAt = (position) => {
+    if (currentHead === null && currentTail === null) return
+    let currentNode = currentHead
     let currentPos = 0
     while (currentNode !== null && currentPos < position) {
       currentNode = currentNode.next
@@ -120,57 +102,113 @@ function DoublyLinkedList() {
     }
     if (position === currentPos) {
       if (currentNode.next === null && currentNode.prev === null) {
-        setNodes([])
+        currentHead = null
+        currentTail = null
       } else if (currentNode.prev === null) {
         currentNode.next.prev = null
+        currentHead = currentNode.next
       } else if (currentNode.next === null) {
         currentNode.prev.next = null
+        currentTail = currentNode.prev
       } else {
         currentNode.next.prev = currentNode.prev
         currentNode.prev.next = currentNode.next
       }
     }
-    setNodes([...nodes.slice(0, position), ...nodes.slice(position + 1)])
+    setHead(currentHead)
+    setTail(currentTail)
   }
 
   const reverse = () => {
-    console.log("reverse")
+    let currentNode = currentHead
+    while (currentNode !== null) {
+      const temp = currentNode.next
+      currentNode.next = currentNode.prev
+      currentNode.prev = temp
+      if (currentNode.next === null) {
+        currentTail = currentNode
+      } else if (currentNode.prev === null) {
+        currentHead = currentNode
+      }
+      currentNode = temp
+    }
+    setHead(currentHead)
+    setTail(currentTail)
   }
 
   const makeUnique = () => {
-    console.log("unique")
+    if (currentHead === null && currentTail === null) return
+    const data = new Set()
+    let currentNode = currentHead
+    while (currentNode !== null) {
+      if (data.has(currentNode.data)) {
+        if (currentNode.next === null) {
+          // delete last element
+          currentNode.prev.next = null
+          currentTail = currentNode.prev
+          currentNode = null
+        } else if (currentNode.prev === null) {
+          // delete first element
+          currentNode.next.prev = null
+          currentHead = currentNode.next
+          currentNode = currentHead
+        } else {
+          currentNode.prev.next = currentNode.next
+          currentNode.next.prev = currentNode.prev
+          currentNode = currentNode.next
+          setRender(!render)
+        }
+      } else {
+        data.add(currentNode.data)
+        currentNode = currentNode.next
+      }
+    }
+    setHead(currentHead)
+    setTail(currentTail)
+  }
+
+  const print = () => {
+    let currentNode = currentHead
+    while (currentNode !== null) {
+      console.log(currentNode.data)
+      currentNode = currentNode.next
+    }
+  }
+
+  const renderList = () => {
+    let currentNode = currentHead
+    const elements = []
+    let currentPos = 0
+    while (currentNode !== null) {
+      const element = <li className="flex flex-col border-2 border-black rounded-md p-4 m-1" key={currentPos}>
+        <p>Data: {currentNode.data}</p>
+        <p>Next: {currentNode.next ? currentNode.next.data : "null"}</p>
+        <p>Prev: {currentNode.prev ? currentNode.prev.data : "null"}</p>
+      </li>
+      elements.push(element)
+      currentNode = currentNode.next
+      currentPos++
+    }
+    return <ul className="flex flex-row flex-wrap flex-shrink">{elements}</ul>
   }
 
   return (
     <div className="p-4 flex flex-col justify-center items-center">
       <h2 className="p-6">Doubly Linked List</h2>
-      <div className="flex flex-row">
-        <div className="flex flex-wrap justify-center items-center max-w-[70%]">
-          {nodes.map((node, index) => <DoublyLinkedListNode key={index} node={node} index={index} />)}
+      <div className="flex flex-row w-[70vw]">
+        <div className="flex flex-wrap w-[70%]">
+          {renderList()}
         </div>
-        <div className="flex flex-col max-w-[30%]">
+        <div className="flex flex-col w-[30%]">
           <h3 className="m-2">Functions</h3>
-          <button onClick={insertAtBeginning}>Insert at Beginning</button>
-          <button onClick={insertAtEnd}>Insert at End</button>
-          <button onClick={insertBefore}>Insert Before</button>
-          <button onClick={deleteAt}>Delete At</button>
+          <button onClick={() => insertAtBeginning(100)}>Insert at Beginning</button>
+          <button onClick={() => insertAtEnd(2)}>Insert at End</button>
+          <button onClick={() => insertBefore(100, 0)}>Insert Before</button>
+          <button onClick={() => deleteAt(0)}>Delete At</button>
           <button onClick={reverse}>Reverse</button>
           <button onClick={makeUnique}>Make Unique</button>
-          <button onClick={printList}>Print List</button>
+          <button onClick={print}>Print</button>
         </div>
-      </div>
-    </div>
-  )
-}
-
-function DoublyLinkedListNode({ node, index }) {
-  return (
-    <div>
-      <div className="flex flex-col p-6 m-2 justify-center items-center border-2 border-black rounded-sm">
-        <p>Next: {node.next ? index + 1 : "null"}</p>
-        <p className="font-bold text-2xl">Data: {node.data}</p>
-        <p>Prev: {node.prev ? index - 1 : "null"}</p>
-        <p>Index: {index}</p>
       </div>
     </div>
   )
